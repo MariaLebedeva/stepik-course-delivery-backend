13b7054e16f653b47841e6442f779a1fc9e4b099
maria <maria.lebedeva@jetbrains.com>
2019-05-30 00:52:35 +0300

    rename routes file

--- flasks/routes.py
+++ flasks/routes.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
@@ -6,6 +7,7 @@ alive_state = False
 workhours_open = "10:00"
 workhours_closes = "22:00"
 promotion_text = "Сегодня скидка 15% по промокоду stepik"
+promocodes =[{"code":"STEPIK","discount":15},{"code":"DELIVERY","discount":10}]
 
 
 @app.route("/alive")
@@ -25,14 +27,10 @@ def promotion():
 
 @app.route("/promo/<code>")
 def checkpromo(code):
-    if code == "stepik":
-        return '{"valid":true,"discount":15}'
-    elif code == "summer":
-        return '{"valid":true,"discount":10}'
-    elif code == "pleaseplease":
-        return '{"valid:true,"discount":5}'
-    else:
-        return '{"valid":false}'
+    for promocode in promocodes:
+        if promocode["code"] == code:
+            return json.dumps()
+    return json.dumps()
 
 
 app.run("0.0.0.0", 8000)
